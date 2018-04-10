25b737850818993d3cfe73d5074b518dccebce07
eddy <edoardolenzi9@gmail.com>
2018-04-10 08:19:40 +0200

    wip

--- lexer.py
+++
@@ -1,8 +0,0 @@
-from domain.models.quick_statement import QuickStatement
-
-
-a = QuickStatement("a", "a", "a", [1], [2], [3], [1], [2], [3])
-b = QuickStatement("ab", "ab", "ab", [11], [12], [13], [11], [12], [13])
-c = []
-c.append(a)
-c.append(b)
